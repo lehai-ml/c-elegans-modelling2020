260c35c4d839bc8371364d2d4d34b46828ffbd4a
Le Hai <hai@Les-MacBook-Pro.local>
2020-02-09 17:35:05 +0000

    updated main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sympy as sp
 from sympy.solvers import solve
 from sympy import symbols, Eq
 import elegansfunc as elegans
+from scipy.integrate import odeint
 
 dataset=pd.read_excel('./dataset.xlsx')
 
@@ -20,22 +21,25 @@ model1=dict({'nsm':neuron_NSM,'asi':neuron_ASI,'adf':neuron_ADF})
 Version1=elegans.Model(model1,by_20C)
 Version1.model['nsm']
 
-##Version3 model Waleed paper:
-##sanity check #Version3 model (Waleed thesis):
-neuron_NSM=elegans.define_model_interactions('nsm',TN2S=-1,DA2S=-1)
-neuron_ADF=elegans.define_model_interactions('adf',TA2S=1,TN2S=-1)
-neuron_ASI=elegans.define_model_interactions('asi',DA2S=1,TA2S=-1)
-model3=dict({'nsm':neuron_NSM,'asi':neuron_ASI,'adf':neuron_ADF})
-Version3=elegans.Model(model3,by_20C)
+##Simulation with odeint
+Y0=[1,1,1]
+t=np.linspace(0,30,1000)
+food=1
+x=odeint(Version1.simulation_in_daf_7mut,Y0,t,args=(food,))
+# ##Version3 model Waleed paper:
+# ##sanity check #Version3 model (Waleed thesis):
+# neuron_NSM=elegans.define_model_interactions('nsm',TN2S=-1,DA2S=-1)
+# neuron_ADF=elegans.define_model_interactions('adf',TA2S=1,TN2S=-1)
+# neuron_ASI=elegans.define_model_interactions('asi',DA2S=1,TA2S=-1)
+# model3=dict({'nsm':neuron_NSM,'asi':neuron_ASI,'adf':neuron_ADF})
+# Version3=elegans.Model(model3,by_20C)
 
 
-neuron_NSM=elegans.define_model_interactions('nsm',TN2S=-1)
-neuron_ADF=elegans.define_model_interactions('adf',TA2S=1,TN2S=-1)
-neuron_ASI=elegans.define_model_interactions('asi',DA2S=1,TA2S=-1)
-model3=dict({'nsm':neuron_NSM,'asi':neuron_ASI,'adf':neuron_ADF})
-Version4=elegans.Model(model3,by_20C)
+# neuron_NSM=elegans.define_model_interactions('nsm',TN2S=-1)
+# neuron_ADF=elegans.define_model_interactions('adf',TA2S=1,TN2S=-1)
+# neuron_ASI=elegans.define_model_interactions('asi',DA2S=1,TA2S=-1)
+# model3=dict({'nsm':neuron_NSM,'asi':neuron_ASI,'adf':neuron_ADF})
 
-# Version4.TA_TN_across_food_levels()
-model3=dict({'nsm':neuron_NSM,'asi':neuron_ASI,'adf':neuron_ADF})
-Version3=elegans.Model(model3,by_20C)
-Version1.model['nsm'].write_equation(print_equation=True)
+
+
+
